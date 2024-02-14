e3936c967ad3fd0fa44f184f3fa61c2b464dc4df
viorel <viorel.chiritoiu@gmail.com>
2024-02-14 16:49:11 +0200

    modify a lot

--- runner.sh
+++ runner.sh
@@ -35,6 +35,6 @@ done
 echo "Selenium Grid is up and running. Running the test...."
 
 # Start the java command
-java -cp "com.cucumber/*;features;libs/*" io.cucumber.core.cli.Main \
+java -cp "com.cucumber/*:features:libs/*" io.cucumber.core.cli.Main \
      -p pretty \
      --glue com/cucumber/stepdefinitions --glue com/cucumber/hooks --glue com/cucumber/customtype features
