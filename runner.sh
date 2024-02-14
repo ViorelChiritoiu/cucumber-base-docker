c15acb1d67f3a999208b46d206358c0ac1b30e8f
viorel <viorel.chiritoiu@gmail.com>
2024-02-14 15:15:30 +0200

    modify a lot

--- runner.sh
+++ runner.sh
@@ -39,7 +39,7 @@ java -cp 'libs/*' \
      -Dselenium.grid.enabled=true \
      -Dselenium.grid.hubHost="${HUB_HOST:-hub}" \
      -Dbrowser="${BROWSER:-chrome}" \
-     -Dthreadcount "${THREAD_COUNT:-1}"
-
-java -cp 'libs/*' io.cucumber.core.cli.Main -p pretty features \
+     -Dthreadcount "${THREAD_COUNT:-1}" \
+     io.cucumber.core.cli.Main \
+     -p pretty features \
      --glue com/cucumber/stepdefinitions --glue com/cucumber/hooks --glue com/cucumber/customtype
